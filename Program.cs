2e50d2a3bb39afd13ee9a5ce44dd4f7c0eb01eff
Kenny Eliasson <kennyeliasson@gmail.com>
2017-02-12 11:32:23 +0100

    Added code to find OR words like A/Z

--- Program.cs
+++ Program.cs
@@ -7,19 +7,17 @@ using System.Text;
 
 namespace WordamentSolver
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var swedishWords = new List<string>();
             foreach (var line in File.ReadLines("dictionary.txt"))
-            {
-                swedishWords.Add(line.Split('/')[0].ToUpper());   
-            }
+                swedishWords.Add(line.Split('/')[0].ToUpper());
 
             var board = new Board(new List<string>
             {
-                "SK|A|D|A",
+                "SK|A/Å|D|A",
                 "X|X|X|X",
                 "X|X|X|X",
                 "X|X|X|X"
@@ -39,9 +37,10 @@ namespace WordamentSolver
         public Board(List<string> lines)
         {
             // Alla linjer lika långa
-            _box = new BoardLetter[lines.Count, lines[0].Split('|').Length];
+            var howManyBoxes = lines[0].Split('|').Length;
+            _box = new BoardLetter[lines.Count, howManyBoxes];
 
-            for (int row = 0; row < lines.Count; row++)
+            for (var row = 0; row < lines.Count; row++)
             {
                 var bricks = lines[row].Split('|');
                 for (var column = 0; column < bricks.Length; column++)
@@ -53,14 +52,11 @@ namespace WordamentSolver
                         Letter = bricks[column]
                     };
 
-                    //_letters[row, column] = lines[row][column];
-
                     _box[row, column] = boardLetter;
                 }
             }
             
             foreach (var letter in _box)
-            {
                 letter.Adjacent = new List<BoardLetter> {
                     GetBoardLetterAtPosition(letter.Column - 1, letter.Row),
                     GetBoardLetterAtPosition(letter.Column + 1, letter.Row),
@@ -71,7 +67,6 @@ namespace WordamentSolver
                     GetBoardLetterAtPosition(letter.Column + 1, letter.Row - 1),
                     GetBoardLetterAtPosition(letter.Column + 1, letter.Row + 1)
                 }.Where(x => x != null).ToList();
-            }
         }
 
         private BoardLetter GetBoardLetterAtPosition(int column, int row )
@@ -94,13 +89,9 @@ namespace WordamentSolver
         public List<string> CreateAllCombinations(Trie<string> validWords)
         {
             var results = new HashSet<string>();
-            for (int row = 0; row < _box.GetLength(0); row++)
-            {
-                for (int column = 0; column < _box.GetLength(1); column++)
-                {
-                    _box[row, column].Build(new HashSet<BoardLetter>(), results, validWords);
-                }
-            }
+            for (var row = 0; row < _box.GetLength(0); row++)
+            for (var column = 0; column < _box.GetLength(1); column++)
+                _box[row, column].Build(new HashSet<BoardLetter>(), results, validWords);
 
             return results.Distinct().OrderBy(x => x.Length).ToList();
         }
@@ -122,7 +113,15 @@ namespace WordamentSolver
             if (word == null)
                 word = new StringBuilder();
 
-            word.Append(Letter);
+            foreach (var letter in Letter.Split('/'))
+            {
+                AppendSingleLetter(letter, visited, foundWords, validWords, new StringBuilder(word.ToString()));
+            }            
+        }
+
+        private void AppendSingleLetter(string appendLetter, HashSet<BoardLetter> visited, HashSet<string> foundWords, Trie<string> validWords, StringBuilder word)
+        {
+            word.Append(appendLetter);
             var currentVisited = new HashSet<BoardLetter>(visited) {this};
 
             var matches = validWords.Retrieve(word.ToString());
@@ -131,17 +130,14 @@ namespace WordamentSolver
 
             var index = matches.BinarySearch(word.ToString());
             if (index >= 0)
-            {
                 foundWords.Add(matches[index]);
-            }
 
             foreach (var letter in Adjacent)
             {
                 if (visited.Contains(letter))
                     continue;
 
-                var currentWord = new StringBuilder(word.ToString());
-                letter.Build(currentVisited, foundWords, validWords, currentWord);
+                letter.Build(currentVisited, foundWords, validWords, word);
             }
         }
 
